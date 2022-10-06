72eeb83be8d03f17ba1046d78647d23d07760e2a
naliaka <esthernaliaka40@gmail.com>
2022-10-06 19:45:39 +0300

    js day_of_birth.js created and updated

---
+++ js/day_of_birth.js
@@ -0,0 +1,13 @@
+var birthdayInput = prompt("ENTER YOUR BIRTHDAY(YYYY-MM-DD): ");
+var genderInput = prompt('ENTER YOUR GENDER (M/F): ').toUpperCase();
+var birthDate = new  Date(birthdayInput);
+var birthDay = birthDate.getDay(); //GETS THE BIRTHDAY FROM THE birthDate
+
+var daysOfWeek = ["Sunday", "Monday", "Tuesday","Wednesday", "Thursday", "Friday", "Saturday"]; 
+// to access day use daysOfWeek[birthDay])
+var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+console.log(femaleNames[birthDay])
+var maleNames = ["Kwasi", "Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+console.log(maleNames[birthDay])
+
+//if (genderInput === "M" || genderInput=== "F") {}
